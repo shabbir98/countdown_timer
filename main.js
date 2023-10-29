f42012db2f03405e9b77e3ffe45f6c1e1916081b
shabbir98 <84226838+shabbir98@users.noreply.github.com>
2023-10-30 01:10:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+async function startCountdown() {
+    let { duration } = await inquirer.prompt({
+        type: 'number',
+        name: 'duration',
+        message: 'Enter the countdown duration in seconds:',
+    });
+    duration *= 1000;
+    let remainingTime = duration;
+    const interval = setInterval(() => {
+        remainingTime -= 1000;
+        const seconds = remainingTime / 1000;
+        if (seconds === 0) {
+            clearInterval(interval);
+            console.log('Countdown finished!');
+        }
+        else {
+            console.clear();
+            console.log(`Time remaining: ${seconds}`);
+        }
+    }, 1000);
+}
+startCountdown();
